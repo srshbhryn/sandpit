b049d2e669212e42ac6a7289fd195e31a805558a
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2023-07-16 14:16:39 +0330

    Binary search tree: Provided 'delete'

--- binary_search_tree/bst.ml
+++ binary_search_tree/bst.ml
@@ -9,6 +9,24 @@ let rec add n = function
   | Empty -> Node (n, Empty, Empty)
   | Node (v, l, r) -> if v >= n then Node (v, add n l, r)  else Node (v, l, add n r)
 
+let rec remove_and_return_least_element = function
+  | Empty -> (Empty, 0)
+  | Node (v, r, Empty) -> (r, v)
+  | Node (v, r, l) -> let rr, m = remove_and_return_least_element l in Node (v, r, rr), m
+
+let rec delete n = function
+  | Empty -> Empty
+  | Node (v, l, r) ->
+    if v > n then
+      Node (v, delete n l, r)
+    else if v < n then
+      Node (v, l, delete n r)
+    else (* n = v *)
+      match l, r with
+      | Empty, r -> r
+      | l, Empty -> l
+      | l, r -> let rr, m = remove_and_return_least_element r in Node (m, l, rr)
+
 let t = add 3 (add 2 (add 1 Empty))
 
 let () = (
@@ -22,5 +40,5 @@ let () = (
     print_endline "1"
   else
     print_endline "no 1"
-
+  ;
 )
